bf25a096460d3ffcec8bc94f6cf66bd447801e36
ranveer4243 <ravudhiman303@gmail.com>
2022-05-03 18:04:45 +0530

    my website

---
+++ index.js
@@ -0,0 +1,16 @@
+var typed = new Typed (".typing", {
+    strings: ["Web Designer..","Devloper..","You Tuber"],
+    typeSpeed: 70,
+    backSpeed:50,
+    loop: true
+});
+var typed = new Typed (".typing-2", {
+    strings: ["Web Designer..","Devloper","You Tuber"],
+    typeSpeed: 100,
+    backSpeed:70,
+    loop: true
+});
+$('.counter').counterUp({
+    delay: 10,
+    time: 1000
+});
